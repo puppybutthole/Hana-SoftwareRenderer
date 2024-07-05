0c3011da8a19d082c36bd5de535f87b902423f0c
ouzhi <ouzhi@udschina.com>
2024-07-05 15:43:06 +0800

    修改读取obj文件的方式

--- Hana-SoftwareRenderer/graphics.cpp
+++ Hana-SoftwareRenderer/graphics.cpp
@@ -1,112 +1,113 @@
-﻿#include <assert.h>
+#include <assert.h>
 #include <math.h>
 #include <stdlib.h>
 #include "graphics.h"
 
 
 static void liner_interpolate_varyings(shader_struct_v2f* from, shader_struct_v2f* to, shader_struct_v2f* ret, int sizeof_varyings, float t) {
-	int num_floats = sizeof_varyings / sizeof(float);
-	float* dst = (float*)ret;
+    int num_floats = sizeof_varyings / sizeof(float);
+    float* dst = (float*)ret;
 
-	float* from_param = (float*)(from);
-	float* to_param = (float*)(to);
+    float* from_param = (float*)(from);
+    float* to_param = (float*)(to);
 
-	for (int i = 0; i < num_floats; i++) {
-		dst[i] = from_param[i] + (to_param[i] - from_param[i]) * t;
-	}
+    for (int i = 0; i < num_floats; i++) {
+        dst[i] = from_param[i] + (to_param[i] - from_param[i]) * t;
+    }
 }
 
 typedef enum {
-	POSITIVE_W,
-	POSITIVE_X,
-	NEGATIVE_X,
-	POSITIVE_Y,
-	NEGATIVE_Y,
-	POSITIVE_Z,
-	NEGATIVE_Z
+    POSITIVE_W,
+    POSITIVE_X,
+    NEGATIVE_X,
+    POSITIVE_Y,
+    NEGATIVE_Y,
+    POSITIVE_Z,
+    NEGATIVE_Z
 } plane_t;
 
 static int is_inside_plane(Vector4f coord, plane_t plane) {
-	switch (plane) {
-	case POSITIVE_W:
-		return coord.w >= EPSILON;
-	case POSITIVE_X:
-		return coord.x <= +coord.w;
-	case NEGATIVE_X:
-		return coord.x >= -coord.w;
-	case POSITIVE_Y:
-		return coord.y <= +coord.w;
-	case NEGATIVE_Y:
-		return coord.y >= -coord.w;
-	case POSITIVE_Z:
-		return coord.z <= +coord.w;
-	case NEGATIVE_Z:
-		return coord.z >= -coord.w;
-	default:
-		assert(0);
-		return 0;
-	}
+    switch (plane) {
+    case POSITIVE_W:
+        return coord.w >= EPSILON;
+    case POSITIVE_X:
+        return coord.x <= +coord.w;
+    case NEGATIVE_X:
+        return coord.x >= -coord.w;
+    case POSITIVE_Y:
+        return coord.y <= +coord.w;
+    case NEGATIVE_Y:
+        return coord.y >= -coord.w;
+    case POSITIVE_Z:
+        return coord.z <= +coord.w;
+    case NEGATIVE_Z:
+        return coord.z >= -coord.w;
+    default:
+        assert(0);
+        return 0;
+    }
 }
 
 static float get_intersect_ratio(Vector4f prev, Vector4f curr, plane_t plane) {
-	switch (plane) {
-	case POSITIVE_W:
-		return (prev.w - EPSILON) / (prev.w - curr.w);
-	case POSITIVE_X:
-		return (prev.w - prev.x) / ((prev.w - prev.x) - (curr.w - curr.x));
-	case NEGATIVE_X:
-		return (prev.w + prev.x) / ((prev.w + prev.x) - (curr.w + curr.x));
-	case POSITIVE_Y:
-		return (prev.w - prev.y) / ((prev.w - prev.y) - (curr.w - curr.y));
-	case NEGATIVE_Y:
-		return (prev.w + prev.y) / ((prev.w + prev.y) - (curr.w + curr.y));
-	case POSITIVE_Z:
-		return (prev.w - prev.z) / ((prev.w - prev.z) - (curr.w - curr.z));
-	case NEGATIVE_Z:
-		return (prev.w + prev.z) / ((prev.w + prev.z) - (curr.w + curr.z));
-	default:
-		assert(0);
-		return 0;
-	}
+    switch (plane) {
+    case POSITIVE_W:
+        return (prev.w - EPSILON) / (prev.w - curr.w);
+    case POSITIVE_X:
+        return (prev.w - prev.x) / ((prev.w - prev.x) - (curr.w - curr.x));
+    case NEGATIVE_X:
+        return (prev.w + prev.x) / ((prev.w + prev.x) - (curr.w + curr.x));
+    case POSITIVE_Y:
+        return (prev.w - prev.y) / ((prev.w - prev.y) - (curr.w - curr.y));
+    case NEGATIVE_Y:
+        return (prev.w + prev.y) / ((prev.w + prev.y) - (curr.w + curr.y));
+    case POSITIVE_Z:
+        return (prev.w - prev.z) / ((prev.w - prev.z) - (curr.w - curr.z));
+    case NEGATIVE_Z:
+        return (prev.w + prev.z) / ((prev.w + prev.z) - (curr.w + curr.z));
+    default:
+        assert(0);
+        return 0;
+    }
 }
 
 static int clip_against_plane(
-	plane_t plane, int in_num_vertices, int varying_num_floats,
-	shader_struct_v2f* in_v2fs,
-	shader_struct_v2f* out_v2fs) {
-	int out_num_vertices = 0;
-	int i, j;
+    plane_t plane, int in_num_vertices, int varying_num_floats,
+    shader_struct_v2f* in_v2fs,
+    shader_struct_v2f* out_v2fs) {
+    int out_num_vertices = 0;
+    int i, j;
 
-	assert(in_num_vertices >= 3);
-	for (i = 0; i < in_num_vertices; i++) {
-		int prev_index = (i - 1 + in_num_vertices) % in_num_vertices;
-		int curr_index = i;
+    assert(in_num_vertices >= 3);
+    for (i = 0; i < in_num_vertices; i++) {
+        int prev_index = (i - 1 + in_num_vertices) % in_num_vertices;
+        int curr_index = i;
 
-		Vector4f prev_coord = in_v2fs[prev_index].clip_pos;
-		Vector4f curr_coord = in_v2fs[curr_index].clip_pos;
+        Vector4f prev_coord = in_v2fs[prev_index].clip_pos;
+        Vector4f curr_coord = in_v2fs[curr_index].clip_pos;
 
-		shader_struct_v2f* prev_v2f = &in_v2fs[prev_index];
-		shader_struct_v2f* curr_v2f = &in_v2fs[curr_index];
+        shader_struct_v2f& prev_v2f = in_v2fs[prev_index];
+        shader_struct_v2f& curr_v2f = in_v2fs[curr_index];
 
-		int prev_inside = is_inside_plane(prev_coord, plane);
-		int curr_inside = is_inside_plane(curr_coord, plane);
+        int prev_inside = is_inside_plane(prev_coord, plane);
+        int curr_inside = is_inside_plane(curr_coord, plane);
 
-		if (prev_inside != curr_inside) {
-			shader_struct_v2f* out_v2f = &out_v2fs[out_num_vertices];
+        if (prev_inside != curr_inside) {
+            shader_struct_v2f& out_v2f = out_v2fs[out_num_vertices];
 
-			float ratio = get_intersect_ratio(prev_coord, curr_coord, plane);
-			liner_interpolate_varyings(prev_v2f, curr_v2f, out_v2f, sizeof(shader_struct_v2f), ratio);
+            float ratio = get_intersect_ratio(prev_coord, curr_coord, plane);
+            liner_interpolate_varyings(&prev_v2f, &curr_v2f, &out_v2f, sizeof(shader_struct_v2f), ratio);
 
-			out_num_vertices += 1;
-		}
+            out_num_vertices += 1;
+        }
 
-		if (curr_inside) {
-			shader_struct_v2f* dest_v2f = &out_v2fs[out_num_vertices];
-			memcpy(dest_v2f, curr_v2f, sizeof(shader_struct_v2f));
-			out_num_vertices += 1;
-		}
-	}
-	return out_num_vertices;
+        if (curr_inside)
+        {
+            shader_struct_v2f& dest_v2f = out_v2fs[out_num_vertices];
+            memcpy(&dest_v2f, &curr_v2f, sizeof(shader_struct_v2f));
+            out_num_vertices += 1;
+        }
+    }
+    return out_num_vertices;
 }
 
 #define CLIP_IN2OUT(plane)                                                  \
@@ -130,34 +131,37 @@ static int clip_against_plane(
     } while (0)
 
 static int is_vertex_visible(Vector4f v) {
-	return fabs(v.x) <= v.w && fabs(v.y) <= v.w && fabs(v.z) <= v.w;
+    return fabs(v.x) <= v.w && fabs(v.y) <= v.w && fabs(v.z) <= v.w;
 }
 
 static int clip_triangle(
-	int sizeof_varyings,
-	shader_struct_v2f* in_v2fs,
-	shader_struct_v2f* out_v2fs) {
-	int v0_visible = is_vertex_visible(in_v2fs[0].clip_pos);
-	int v1_visible = is_vertex_visible(in_v2fs[1].clip_pos);
-	int v2_visible = is_vertex_visible(in_v2fs[2].clip_pos);
-	if (v0_visible && v1_visible && v2_visible) {
-		memcpy(&out_v2fs[0], &in_v2fs[0], sizeof(shader_struct_v2f));
-		memcpy(&out_v2fs[1], &in_v2fs[1], sizeof(shader_struct_v2f));
-		memcpy(&out_v2fs[2], &in_v2fs[2], sizeof(shader_struct_v2f));
-		return 3;
-	}
-	else {
-		int varying_num_floats = sizeof_varyings / sizeof(float);
-		int num_vertices = 3;
-		CLIP_IN2OUT(POSITIVE_W);
-		CLIP_OUT2IN(POSITIVE_X);
-		CLIP_IN2OUT(NEGATIVE_X);
-		CLIP_OUT2IN(POSITIVE_Y);
-		CLIP_IN2OUT(NEGATIVE_Y);
-		CLIP_OUT2IN(POSITIVE_Z);
-		CLIP_IN2OUT(NEGATIVE_Z);
-		return num_vertices;
-	}
+    int sizeof_varyings,
+    shader_struct_v2f* in_v2fs,
+    shader_struct_v2f* out_v2fs)
+{
+    int v0_visible = is_vertex_visible(in_v2fs[0].clip_pos);
+    int v1_visible = is_vertex_visible(in_v2fs[1].clip_pos);
+    int v2_visible = is_vertex_visible(in_v2fs[2].clip_pos);
+    if (v0_visible && v1_visible && v2_visible)
+    {
+        memcpy(&out_v2fs[0], &in_v2fs[0], sizeof(shader_struct_v2f));
+        memcpy(&out_v2fs[1], &in_v2fs[1], sizeof(shader_struct_v2f));
+        memcpy(&out_v2fs[2], &in_v2fs[2], sizeof(shader_struct_v2f));
+        return 3;
+    }
+    else
+    {
+        int varying_num_floats = sizeof_varyings / sizeof(float);
+        int num_vertices = 3;
+        CLIP_IN2OUT(POSITIVE_W);
+        CLIP_OUT2IN(POSITIVE_X);
+        CLIP_IN2OUT(NEGATIVE_X);
+        CLIP_OUT2IN(POSITIVE_Y);
+        CLIP_IN2OUT(NEGATIVE_Y);
+        CLIP_OUT2IN(POSITIVE_Z);
+        CLIP_IN2OUT(NEGATIVE_Z);
+        return num_vertices;
+    }
 }
 
 /*
@@ -170,13 +174,13 @@ static int clip_triangle(
  *     return vec3_cross(ab, ac).z <= 0;
  */
 static bool is_back_facing(Vector3f* ndc_coords) {
-	Vector3f a = ndc_coords[0];
-	Vector3f b = ndc_coords[1];
-	Vector3f c = ndc_coords[2];
-	float signed_area = a.x * b.y - a.y * b.x +
-		b.x * c.y - b.y * c.x +
-		c.x * a.y - c.y * a.x;
-	return signed_area <= 0;
+    Vector3f a = ndc_coords[0];
+    Vector3f b = ndc_coords[1];
+    Vector3f c = ndc_coords[2];
+    float signed_area = a.x * b.y - a.y * b.x +
+        b.x * c.y - b.y * c.x +
+        c.x * a.y - c.y * a.x;
+    return signed_area <= 0;
 }
 
 /*
@@ -184,13 +188,13 @@ static bool is_back_facing(Vector3f* ndc_coords) {
  * https://www.khronos.org/registry/OpenGL/specs/es/2.0/es_full_spec_2.0.pdf
  */
 static float interpolate_depth(float* screen_depths, Vector3f weights) {
-	Vector3f screen_depth;
-	for (size_t i = 0; i < 3; i++)
-	{
-		screen_depth[i] = screen_depths[i];
-	}
+    Vector3f screen_depth;
+    for (size_t i = 0; i < 3; i++)
+    {
+        screen_depth[i] = screen_depths[i];
+    }
 
-	return screen_depth * weights;
+    return screen_depth * weights;
 }
 
 /*
@@ -203,33 +207,33 @@ static float interpolate_depth(float* screen_depths, Vector3f weights) {
  */
 
 static void interpolate_varyings(shader_struct_v2f* v2f, shader_struct_v2f* ret, int sizeof_varyings, Vector3f weights, float recip_w[3]) {
-	int num_floats = sizeof_varyings / sizeof(float);
-	float* src0 = (float*)(&v2f[0]);
-	float* src1 = (float*)(&v2f[1]);
-	float* src2 = (float*)(&v2f[2]);
-	float* dst = (float*)ret;
-	float weight0 = recip_w[0] * weights.x;
-	float weight1 = recip_w[1] * weights.y;
-	float weight2 = recip_w[2] * weights.z;
-	float normalizer = 1 / (weight0 + weight1 + weight2);
-	int i;
-	for (i = 0; i < num_floats; i++) {
-		float sum = src0[i] * weight0 + src1[i] * weight1 + src2[i] * weight2;
-		dst[i] = sum * normalizer;
-	}
+    int num_floats = sizeof_varyings / sizeof(float);
+    float* src0 = (float*)(&v2f[0]);
+    float* src1 = (float*)(&v2f[1]);
+    float* src2 = (float*)(&v2f[2]);
+    float* dst = (float*)ret;
+    float weight0 = recip_w[0] * weights.x;
+    float weight1 = recip_w[1] * weights.y;
+    float weight2 = recip_w[2] * weights.z;
+    float normalizer = 1 / (weight0 + weight1 + weight2);
+    int i;
+    for (i = 0; i < num_floats; i++) {
+        float sum = src0[i] * weight0 + src1[i] * weight1 + src2[i] * weight2;
+        dst[i] = sum * normalizer;
+    }
 }
 
 static Vector3f barycentric(Vector2f A, Vector2f B, Vector2f C, Vector2f P) {
-	Vector3f s[2];
-	for (int i = 2; i--; ) {
-		s[i][0] = C[i] - A[i];
-		s[i][1] = B[i] - A[i];
-		s[i][2] = A[i] - P[i];
-	}
-	Vector3f u = cross(s[0], s[1]);
-	if (std::abs(u[2]) > 1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
-		return Vector3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
-	return Vector3f(-1, 1, 1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
+    Vector3f s[2];
+    for (int i = 2; i--; ) {
+        s[i][0] = C[i] - A[i];
+        s[i][1] = B[i] - A[i];
+        s[i][2] = A[i] - P[i];
+    }
+    Vector3f u = cross(s[0], s[1]);
+    if (std::abs(u[2]) > 1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
+        return Vector3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+    return Vector3f(-1, 1, 1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
 }
 
 //扫描线法
@@ -311,106 +315,114 @@ static Vector3f barycentric(Vector2f A, Vector2f B, Vector2f C, Vector2f P) {
 //	}
 //}
 
-static void rasterize_triangle(DrawData* draw_data, shader_struct_v2f* v2f) {
-	// 齐次除法 / 透视除法 (homogeneous division / perspective division)
-	Vector3f ndc_coords[3];
-	for (int i = 0; i < 3; i++) ndc_coords[i] = proj<3>(v2f[i].clip_pos / v2f[i].clip_pos[3]);
+static void rasterize_triangle(DrawData* draw_data, shader_struct_v2f* v2f)
+{
+    // 齐次除法 / 透视除法 (homogeneous division / perspective division)
+    Vector3f ndc_coords[3];
+    for (int i = 0; i < 3; i++) ndc_coords[i] = proj<3>(v2f[i].clip_pos / v2f[i].clip_pos[3]);
 
-	// 背面剔除
-	if (is_back_facing(ndc_coords)) return;
+    // 背面剔除
+    if (is_back_facing(ndc_coords)) return;
 
-	RenderBuffer* render_buffer = draw_data->render_buffer;
+    RenderBuffer* render_buffer = draw_data->render_buffer;
 
-	// 屏幕映射
-	Vector2f screen_coords[3];
-	float screen_depth[3];
-	for (int i = 0; i < 3; i++) {
-		Vector3f win_coord = viewport_transform(render_buffer->width, render_buffer->height, ndc_coords[i]);
-		screen_coords[i] = Vector2f(win_coord.x, win_coord.y);
-		screen_depth[i] = win_coord.z;
-	}
+    // 屏幕映射
+    Vector2f screen_coords[3];
+    float screen_depth[3]{};
+    for (int i = 0; i < 3; i++)
+    {
+        Vector3f win_coord = viewport_transform(render_buffer->width, render_buffer->height, ndc_coords[i]);
+        screen_coords[i] = Vector2f(win_coord.x, win_coord.y);
+        screen_depth[i] = win_coord.z;
+    }
 
-	float recip_w[3];
-	/* reciprocals of w */
-	for (int i = 0; i < 3; i++) {
-		recip_w[i] = 1 / v2f[i].clip_pos[3];
-	}
+    float recip_w[3]{};
+    /* reciprocals of w */
+    for (int i = 0; i < 3; i++)
+    {
+        recip_w[i] = 1 / v2f[i].clip_pos[3];
+    }
 
-	Vector2f bboxmin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
-	Vector2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
-	Vector2f clamp(render_buffer->width - 1, render_buffer->height - 1);
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 2; j++) {
-			bboxmin[j] = std::max(0.f, std::min(bboxmin[j], screen_coords[i][j]));
-			bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], screen_coords[i][j]));
-		}
-	}
+    Vector2f bboxmin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+    Vector2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+    Vector2f clamp(render_buffer->width - 1, render_buffer->height - 1);
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            bboxmin[j] = std::max(0.f, std::min(bboxmin[j], screen_coords[i][j]));
+            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], screen_coords[i][j]));
+        }
+    }
 
 //#pragma omp parallel for
-	for (int x = bboxmin.x; x <= int(bboxmax.x); x++) {
-		for (int y = bboxmin.y; y <= int(bboxmax.y); y++) {
-			Vector2i P;
-			P.x = x; P.y = y;
-			Vector3f barycentric_weights = barycentric(screen_coords[0], screen_coords[1], screen_coords[2], P);
-			if (barycentric_weights.x < 0 || barycentric_weights.y < 0 || barycentric_weights.z < 0) continue;
+    Vector2i P;
+    for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++)
+    {
+        for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++)
+        {
+            Vector3f barycentric_weights = barycentric(screen_coords[0], screen_coords[1], screen_coords[2], P);
+            if (barycentric_weights.x < 0 || barycentric_weights.y < 0 || barycentric_weights.z < 0) continue;
 
-			// 深度插值
-			float frag_depth = interpolate_depth(screen_depth, barycentric_weights);
+            // 深度插值
+            float frag_depth = interpolate_depth(screen_depth, barycentric_weights);
 
-			// 深度测试
-			if (frag_depth > render_buffer->get_depth(P.x, P.y)) continue;
+            
+            // 深度测试
+            float depth = render_buffer->get_depth(P.x, P.y);
+            if (frag_depth > depth) continue;
 
-			// 变量插值
-			shader_struct_v2f interpolate_v2f;
-			interpolate_varyings(v2f, &interpolate_v2f, sizeof(shader_struct_v2f), barycentric_weights, recip_w);
+            // 变量插值
+            shader_struct_v2f interpolate_v2f;
+            interpolate_varyings(v2f, &interpolate_v2f, sizeof(shader_struct_v2f), barycentric_weights, recip_w);
 
-			// fragment shader
-			Color color = Color::White;
-			color.r *= frag_depth;
-			color.g *= frag_depth;
-			color.b *= frag_depth;
-			//bool discard = draw_data->shader->fragment(&interpolate_v2f, color);
+            // fragment shader
+            Color color = Color::Green;
+            //bool discard = draw_data->shader->fragment(&interpolate_v2f, color);
 
-			// 绘制像素
-			//if (!discard) 
-			{
-				render_buffer->set_depth(P.x, P.y, frag_depth);
-				render_buffer->set_color(P.x, P.y, color);
-			}
-		}
-	}
+            // 绘制像素
+            //if (!discard)
+            {
+                render_buffer->set_depth(P.x, P.y, frag_depth);
+                render_buffer->set_color(P.x, P.y, color);
+            }
+        }
+    }
+
+    return;
 }
 
-void graphics_draw_triangle(DrawData* draw_data) {
-	
-	for (int i = 0; i < draw_data->model->nfaces(); i++)
-	{
-		shader_struct_v2f v2fs[3];
-		for (int j = 0; j < 3; j++) 
-		{
-			shader_struct_a2v a2v;
-			a2v.obj_pos = draw_data->model->vert(i, j);
-			//a2v.obj_normal = draw_data->model->normal(i, j);
-			//a2v.uv = draw_data->model->uv(i, j);
-			v2fs[j] = draw_data->shader->vertex(&a2v);
-		}
+void graphics_draw_triangle(DrawData* draw_data)
+{
+    int nfaces = draw_data->model->nfaces();
+    for (int i = 0; i < nfaces; ++i)
+    {
+        shader_struct_v2f v2fs[3] = {};
+        for (int j = 0; j < 3; ++j)
+        {
+            shader_struct_a2v a2v = {};
+            a2v.obj_pos = draw_data->model->vert(i, j);
+            //a2v.obj_normal = draw_data->model->normal(i, j);
+            //a2v.uv = draw_data->model->uv(i, j);
+            v2fs[j] = draw_data->shader->vertex(&a2v);
+        }
 
-		shader_struct_v2f clip_v2fs[3 * 10];
+        shader_struct_v2f clip_v2fs[3 * 10] = {};
+        int num_vertices = clip_triangle(0, v2fs, clip_v2fs);
 
-		int num_vertices = clip_triangle(0, v2fs, clip_v2fs);
+        /* triangle assembly */
+        for (int j = 0; j < num_vertices - 2; ++j)
+        {
+            int index0 = 0;
+            int index1 = j + 1;
+            int index2 = j + 2;
 
-		/* triangle assembly */
-		for (int j = 0; j < num_vertices - 2; j++) {
-			int index0 = 0;
-			int index1 = j + 1;
-			int index2 = j + 2;
+            shader_struct_v2f ret_v2fs[3];
+            ret_v2fs[0] = clip_v2fs[index0];
+            ret_v2fs[1] = clip_v2fs[index1];
+            ret_v2fs[2] = clip_v2fs[index2];
 
-			shader_struct_v2f ret_v2fs[3];
-			ret_v2fs[0] = clip_v2fs[index0];
-			ret_v2fs[1] = clip_v2fs[index1];
-			ret_v2fs[2] = clip_v2fs[index2];
-
-			rasterize_triangle(draw_data, ret_v2fs);
-		}
-	}
+            rasterize_triangle(draw_data, ret_v2fs);
+        }
+    }
 }
--- Hana-SoftwareRenderer/scene.cpp
+++ Hana-SoftwareRenderer/scene.cpp
@@ -1,147 +1,148 @@
-﻿#include "scene.h"
+#include "scene.h"
 
 Scene::Scene(RenderBuffer* render_Buffer) {
-	this->frameBuffer = render_Buffer;
-	float aspect = (float)this->frameBuffer->width / (float)this->frameBuffer->height;
-	camera = new Camera(CAMERA_POSITION, CAMERA_TARGET, aspect);
-	light = new GameObject(Vector3f(2, 2, 2));
-	enable_shadow = true;
+    this->frameBuffer = render_Buffer;
+    float aspect = (float)this->frameBuffer->width / (float)this->frameBuffer->height;
+    camera = new Camera(CAMERA_POSITION, CAMERA_TARGET, aspect);
+    light = new GameObject(Vector3f(2, 2, 2));
+    enable_shadow = true;
 }
 
 void Scene::tick(float delta_time) {
-	Vector3f camera_pos = camera->get_position();
-	Vector3f camera_dir = camera->get_forward();
+    Vector3f camera_pos = camera->get_position();
+    Vector3f camera_dir = camera->get_forward();
 
-	Vector3f light_pos = light->transform.position;
-	Vector3f target_pos = camera->get_target_position();
-	Vector3f light_dir = (light_pos - target_pos).normalize();
+    Vector3f light_pos = light->transform.position;
+    Vector3f target_pos = camera->get_target_position();
+    Vector3f light_dir = (light_pos - target_pos).normalize();
 
-	snprintf(text, 500, "");
+    snprintf(text, 500, "");
 
-	char line[50] = "";
-	snprintf(line, 50, "camera pos: (%.1f, %.1f, %.1f)\n", camera_pos.x, camera_pos.y, camera_pos.z);
-	strcat(text, line);
-	snprintf(line, 50, "camera dir: (%.1f, %.1f, %.1f)\n", TO_DEGREES(camera_dir.x), TO_DEGREES(camera_dir.y), TO_DEGREES(camera_dir.z));
-	strcat(text, line);
-	snprintf(line, 50, "press mouse [Left] to rotate camera\n");
-	strcat(text, line);
-	snprintf(line, 50, "press mouse [Right] to move camera\n");
-	strcat(text, line);
-	snprintf(line, 50, "press key [Space] to reset camera\n\n");
-	strcat(text, line);
+    char line[50] = "";
+    snprintf(line, 50, "camera pos: (%.1f, %.1f, %.1f)\n", camera_pos.x, camera_pos.y, camera_pos.z);
+    strcat(text, line);
+    snprintf(line, 50, "camera dir: (%.1f, %.1f, %.1f)\n", TO_DEGREES(camera_dir.x), TO_DEGREES(camera_dir.y), TO_DEGREES(camera_dir.z));
+    strcat(text, line);
+    snprintf(line, 50, "press mouse [Left] to rotate camera\n");
+    strcat(text, line);
+    snprintf(line, 50, "press mouse [Right] to move camera\n");
+    strcat(text, line);
+    snprintf(line, 50, "press key [Space] to reset camera\n\n");
+    strcat(text, line);
 
 
-	snprintf(line, 50, "light dir: (%.1f, %.1f, %.1f)\n", TO_DEGREES(light_dir.x), TO_DEGREES(light_dir.y), TO_DEGREES(light_dir.z));
-	strcat(text, line);
-	snprintf(line, 50, "press key [A] or [D] to rotate light dir\n\n");
-	strcat(text, line);
+    snprintf(line, 50, "light dir: (%.1f, %.1f, %.1f)\n", TO_DEGREES(light_dir.x), TO_DEGREES(light_dir.y), TO_DEGREES(light_dir.z));
+    strcat(text, line);
+    snprintf(line, 50, "press key [A] or [D] to rotate light dir\n\n");
+    strcat(text, line);
 
-	snprintf(line, 50, "shadow: %s\n", enable_shadow ? "On" : "Off");
-	strcat(text, line);
-	snprintf(line, 50, "press key [E] to switch shadow\n\n");
-	strcat(text, line);
+    snprintf(line, 50, "shadow: %s\n", enable_shadow ? "On" : "Off");
+    strcat(text, line);
+    snprintf(line, 50, "press key [E] to switch shadow\n\n");
+    strcat(text, line);
 }
 
 void Scene::on_key_input(keycode_t key, int pressed) {
-	if (pressed)
-	{
-		switch (key)
-		{
-		case KEY_A:
-			light->transform.position = proj<3>(rotate(TO_RADIANS(-5), 0, 1, 1) * embed<4>(light->transform.position, 1.f));
-			break;
-		case KEY_D:
-			light->transform.position = proj<3>(rotate(TO_RADIANS(5), 0, 1, 1) * embed<4>(light->transform.position, 1.f));
-			break;
-		case KEY_E:
-			enable_shadow = !enable_shadow;
-			break;
-		default:
-			break;
-		}
-	}
+    if (pressed)
+    {
+        switch (key)
+        {
+        case KEY_A:
+            light->transform.position = proj<3>(rotate(TO_RADIANS(-5), 0, 1, 1) * embed<4>(light->transform.position, 1.f));
+            break;
+        case KEY_D:
+            light->transform.position = proj<3>(rotate(TO_RADIANS(5), 0, 1, 1) * embed<4>(light->transform.position, 1.f));
+            break;
+        case KEY_E:
+            enable_shadow = !enable_shadow;
+            break;
+        default:
+            break;
+        }
+    }
 }
 
 char* Scene::get_text() {
-	return text;
+    return text;
 }
 
 Scene::~Scene() {
-	delete camera;
-	delete light;
+    delete camera;
+    delete light;
 }
 
 SingleModelScene::SingleModelScene(const char* file_name, RenderBuffer* render_Buffer) :Scene(render_Buffer) {
-	gameobject = new GameObject_StaticModel(file_name);
+    gameobject = new GameObject_StaticModel(file_name);
 
-	material = new Material();
-	material->diffuse_map = gameobject->model->get_diffuse_map();
-	material->normal_map = gameobject->model->get_normal_map();
-	material->specular_map = gameobject->model->get_specular_map();
-	material->color = Color::White;
-	material->specular = Color::White;
-	material->gloss = 50;
-	material->bump_scale = 1;
+    material = new Material();
+    material->diffuse_map = gameobject->model->get_diffuse_map();
+    material->normal_map = gameobject->model->get_normal_map();
+    material->specular_map = gameobject->model->get_specular_map();
+    material->color = Color::White;
+    material->specular = Color::White;
+    material->gloss = 50;
+    material->bump_scale = 1;
 
-	shaderInfos[5].des = "6.NormalMap";
-	shaderInfos[5].shader = new NormalMapShader();
-	shaderInfos[1].des = "5.Blinn";
-	shaderInfos[1].shader = new BlinnShader();
-	shaderInfos[2].des = "4.Texture_Light(no shadow)";
-	shaderInfos[2].shader = new TextureWithLightShader();
-	shaderInfos[3].des = "3.Texture(no shadow)";
-	shaderInfos[3].shader = new TextureShader();
-	shaderInfos[4].des = "2.Toon(no shadow)";
-	shaderInfos[4].shader = new ToonShader();
-	shaderInfos[0].des = "1.Ground(no shadow)";
-	shaderInfos[0].shader = new GroundShader();
+    //shaderInfos[0].des = "6.NormalMap";
+    //shaderInfos[0].shader = new NormalMapShader();
+    //shaderInfos[1].des = "5.Blinn";
+    //shaderInfos[1].shader = new BlinnShader();
+    //shaderInfos[2].des = "4.Texture_Light(no shadow)";
+    //shaderInfos[2].shader = new TextureWithLightShader();
+    //shaderInfos[3].des = "3.Texture(no shadow)";
+    //shaderInfos[3].shader = new TextureShader();
+    //shaderInfos[4].des = "2.Toon(no shadow)";
+    //shaderInfos[4].shader = new ToonShader();
+    shaderInfos[0].des = "1.Ground(no shadow)";
+    shaderInfos[0].shader = new GroundShader();
 
 
-	cur_shader_index = 0;
+    cur_shader_index = 0;
 
-	draw_model = new DrawModel(light, gameobject, material, shaderInfos[cur_shader_index].shader);
+    draw_model = new DrawModel(light, gameobject, material, shaderInfos[cur_shader_index].shader);
 }
 
 SingleModelScene::~SingleModelScene() {
-	delete draw_model;
-	delete material;
-	delete gameobject;
-	for (int i = 0; i < 6; i++)
-	{
-		delete shaderInfos[i].shader;
-	}
+    delete draw_model;
+    delete material;
+    delete gameobject;
+    for (int i = 0; i < 1; i++)
+    {
+        delete shaderInfos[i].shader;
+    }
 }
 
 void SingleModelScene::tick(float delta_time) {
-	Scene::tick(delta_time);
+    Scene::tick(delta_time);
 
-	char line[50] = "";
-	snprintf(line, 50, "shader: %s\n", shaderInfos[cur_shader_index].des);
-	strcat(text, line);
-	snprintf(line, 50, "press key [Q] to switch shader\n\n");
-	strcat(text, line);
+    char line[50] = "";
+    snprintf(line, 50, "shader: %s\n", shaderInfos[cur_shader_index].des);
+    strcat(text, line);
+    snprintf(line, 50, "press key [Q] to switch shader\n\n");
+    strcat(text, line);
 
-	draw_model->draw(camera, frameBuffer, enable_shadow);
+    draw_model->draw(camera, frameBuffer, enable_shadow);
 }
 
-void SingleModelScene::on_key_input(keycode_t key, int pressed) {
-	Scene::on_key_input(key, pressed);
+void SingleModelScene::on_key_input(keycode_t key, int pressed)
+{
+    Scene::on_key_input(key, pressed);
 
-	/*if (pressed)
-	{
-		switch (key)
-		{
-		case KEY_Q:
-			delete draw_model;
-			cur_shader_index = (cur_shader_index + 1 + 6) % 6;
-			if (cur_shader_index >= 3) enable_shadow = false;
-			else enable_shadow = true;
-			draw_model = new DrawModel(light, gameobject, material, shaderInfos[cur_shader_index].shader);
+    //if (pressed)
+    //{
+    //    switch (key)
+    //    {
+    //    case KEY_Q:
+    //        delete draw_model;
+    //        cur_shader_index = (cur_shader_index + 1 + 1) % 1;
+    //        if (cur_shader_index >= 3) enable_shadow = false;
+    //        else enable_shadow = true;
+    //        draw_model = new DrawModel(light, gameobject, material, shaderInfos[cur_shader_index].shader);
 
 
-			break;
-		default:
-			break;
-		}
-	}*/
+    //        break;
+    //    default:
+    //        break;
+    //    }
+    //}
 }
